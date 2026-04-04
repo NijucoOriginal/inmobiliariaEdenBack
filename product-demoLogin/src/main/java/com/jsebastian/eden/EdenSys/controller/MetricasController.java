dfef8edb33e6911a075d7380db3b56ffdeb12371
Luis Carlos Morales C. <luisc.moralesc@uqvirtual.edu.co>
2026-04-04 01:57:15 -0500

    Cambio

---
+++ product-demoLogin/src/main/java/com/jsebastian/eden/EdenSys/controller/MetricasController.java
@@ -0,0 +1,28 @@
+package com.jsebastian.eden.EdenSys.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api/metricas")
+@CrossOrigin
+public class MetricasController {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${prometheus.url:http://prometheus:9090}")
+    private String prometheusUrl;
+
+    @GetMapping("/query")
+    public ResponseEntity<String> query(@RequestParam String query) {
+        try {
+            String url = prometheusUrl + "/api/v1/query?query=" + query;
+            String response = restTemplate.getForObject(url, String.class);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.ok("{\"data\":{\"result\":[]}}");
+        }
+    }
+}
